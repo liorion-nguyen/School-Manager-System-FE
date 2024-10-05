46c4ec5b7e0d5a906d7670895c8a42725f834c4d
buiducccc <bduc0608@gmail.com>
2024-10-05 23:54:06 +0700

    code login

--- app/src/main/java/com/example/schoolmanagersystem/MainActivity.java
+++ app/src/main/java/com/example/schoolmanagersystem/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.schoolmanagersystem;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText inputName, inputPassword;
+    Button btnLogin, btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.login);
+        inputName = findViewById(R.id.inputName);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (inputName.getText().toString().trim() != "" && inputPassword.getText().toString().trim() != "") {
+                    Toast.makeText(MainActivity.this, "Ban da dang nhap thanh cong", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Ban chua dien day du thong tin", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
